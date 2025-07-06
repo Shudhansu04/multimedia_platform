f45302aa4abceb569354df548acc67eae5c48361
SHUDHANSU PANDEY <shudhansupandey2004@gmail.com>
2025-07-07 00:05:21 +0530

    fix: enhance error handling in asyncHandler to validate error codes

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -3,7 +3,9 @@ const asyncHandler = (requestHandler) => {
         try {
             await requestHandler(req, res, next);
         } catch (error) {
-            const statusCode = typeof error.code === "number" ? error.code : 500;
+            const statusCode = (typeof error.code === "number" && error.code >= 100 && error.code < 600)
+        ? error.code
+        : 500;
             res.status(statusCode).json({
                 success: false,
                 message: error.message || "Something went wrong"
