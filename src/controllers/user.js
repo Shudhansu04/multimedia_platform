b19af7b5722337968c34afe4f06f0c98158220a4
SHUDHANSU PANDEY <shudhansupandey2004@gmail.com>
2025-07-06 22:50:14 +0530

    feat: add getWatchHistory function and improve formatting in getUserChannelProfile

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -340,21 +340,68 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
             $project: {
                 fullname: 1,
                 username: 1,
-                subscribersCount:1,
-                channelsSubscribedToCount:1,
-                isSubscribed:1,
-                avatar:1,
-                coverImage:1,
-                email:1
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
 
             }
         }
     ])
-    if(!channel?.length){
-        throw new ApiError(404,"channel doesn't exists")
+    if (!channel?.length) {
+        throw new ApiError(404, "channel doesn't exists")
     }
-    return res.status(200).json(new ApiResponse(200,channel[0],"user channel fetched successfully"))
+    return res.status(200).json(new ApiResponse(200, channel[0], "user channel fetched successfully"))
 })
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1,
 
-export { registerUser, loginUser, loggedOutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateAvatar, updateCoverImage, getUserChannelProfile };
+                                    }
+                                },
+                                {
+                                    $addFields: {
+                                        owner: {
+                                            $first: "$owner"
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+
+        }
+    ])
+
+    return res.status(200).json(new ApiResponse(200,user[0].watchHistory,"Watch history fetched successfully"))
+})
+
+export { registerUser, loginUser, loggedOutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateAvatar, updateCoverImage, getUserChannelProfile, getWatchHistory };
