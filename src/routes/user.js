322641042fb49997dd7093ff8896d384f21bb178
SHUDHANSU PANDEY <shudhansupandey2004@gmail.com>
2025-07-09 22:19:48 +0530

    fix: update user route imports and formatting for consistency

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import { registerUser, loginUser, loggedOutUser, refreshAccessToken,changeCurrentPassword,getCurrentUser ,updateAccountDetails,updateAvatar,updateCoverImage,getUserChannelProfile,getWatchHistory} from "../controllers/user.js";
+import { registerUser, loginUser, loggedOutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateAvatar, updateCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.js";
 import { upload } from "../middlewares/multer.js";
 import { verifyJWT } from "../middlewares/auth.js"
 
@@ -27,15 +27,15 @@ router.route("/refresh-token").post(refreshAccessToken)
 
 router.route("/logout").post(verifyJWT, loggedOutUser)
 
-router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 
-router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
 
-router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
-router.route("/avatar-update").patch(verifyJWT,upload.single("avatar"),updateAvatar)
+router.route("/avatar-update").patch(verifyJWT, upload.single("avatar"), updateAvatar)
 
-router.route("/cover-image-update").patch(verifyJWT,upload.single("coverImage"),updateCoverImage)
+router.route("/cover-image-update").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 
